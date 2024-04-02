e8a0012be7062fc052570d27816f51ae4dd4dae1
zachkoverman <zachkoverman@gmail.com>
2024-04-01 21:10:49 -0700

    My 1st, commit, creating and setting up file to work on problem set

---
+++ problem_set_1.py
@@ -0,0 +1,28 @@
+"""
+Zach Koverman
+4/1/24
+ECON 481
+Problem Set 1
+
+This file contains functions written as exercises for Problem Set 1 in
+ECON 481 - Data Science Computing for Economics
+"""
+
+def github() -> str:
+    """
+    Exercise 0. Returns the URL to this file in my GitHub repository for this
+    class.
+    """
+
+    return "https://github.com/zachkoverman/econ481"
+
+def main():
+    """
+    Calls each function to test my solutions to the exercises in this 
+    problem set.
+    """
+    
+
+if __name__ == "__main__":
+    main()
+ 
