53c8da022dd683c4d79d71c7bd15d4acd4f2f5a3
zachkoverman <zachkoverman@gmail.com>
2024-04-02 21:48:51 -0700

    Completed exercises 3 and 4, added new demonstrations/tests in main method, set up shell for exercise 5

--- problem_set_1.py
+++ problem_set_1.py
@@ -7,6 +7,8 @@ Problem Set 1
 This file contains functions written as exercises for Problem Set 1 in
 ECON 481 - Data Science Computing for Economics
 """
+from typing import Union
+from datetime import datetime
 
 def github() -> str:
     """
@@ -33,13 +35,60 @@ def evens_and_odds(n: int) -> dict:
 
     return sums_dict
 
+def time_diff(date_1: str, date_2: str, out: str="float") -> Union[str,float]:
+    """
+    Exercise 3 - Takes two strings representing dates in the format 
+    "YYYY-MM-DD". Argument "out" specifies how to format the return: when 
+    "float", it returns the number of days between the two dates. If "string",
+    it returns a string containing a sentence telling the difference in days.
+    """
+    dt_1 = datetime.strptime(date_1, "%Y-%m-%d")
+    dt_2 = datetime.strptime(date_2, "%Y-%m-%d")
+    delta = dt_1 - dt_2
+
+    if out == "float":
+        output = abs(delta.days)
+    elif out == "string":
+        output = f"There are {abs(delta.days)} days between the two dates."
+
+    return output
+
+def reverse(in_list: list) -> list:
+    """
+    Exercise 4 - Takes a list and returns the same elements in reversed order.
+    """
+    out_list = []
+    for i in range(1, len(in_list) + 1):
+        out_list.append(in_list[-i])
+
+    return out_list
+
+def prob_k_heads(n: int, k: int) -> float:
+    """
+    Exercise 5 - 
+    """
+
+    return None
+
 def main():
     """
-    Calls each function to test my solutions to the exercises in this 
+    Calls each function to demonstrate my solutions to the exercises in this 
     problem set.
     """
+    # Exercise 2
     print(evens_and_odds(4))
+    print(evens_and_odds(9))
+
+    # Exercise 3
+    print(time_diff("2024-04-02", "2001-12-20"))
+    print(time_diff("2001-12-20", "2024-04-02", "string"))
+    print(time_diff("2024-04-01", "2024-04-02", "string"))
+
+    # Exercise 4
+    print(reverse(['a', 'b', 'c']))
+    print(reverse([]))
+    print(reverse(['a']))
+    print(reverse(['a', 'b', 'c', 'd', 'e', 'f']))
 
 if __name__ == "__main__":
     main()
- 
