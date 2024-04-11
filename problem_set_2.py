3a8a409dd61df4cc6baf6122ff69ad2be63c53d1
zachkoverman <zachkoverman@gmail.com>
2024-04-10 22:59:23 -0700

    Finalized for submission, finishing docstrings

--- problem_set_2.py
+++ problem_set_2.py
@@ -36,16 +36,17 @@ def simulate_data(seed: int=481) -> tuple:
     x_arr[:, 1] = rng.normal(0, np.sqrt(2), 1000)
     x_arr[:, 2] = rng.normal(0, np.sqrt(2), 1000)
     epsilon = rng.normal(0, 1, 1000)
-    
+
     y_arr = 5 + (3 * x_arr[:, 0]) + (2 * x_arr[:, 1]) \
             + (6 * x_arr[:, 2]) + epsilon
     y_arr = np.reshape(y_arr, (1000, 1))
-    
+
     return y_arr, x_arr
 
 def negative_ll(beta: np.array, y_arr: np.array, x_arr: np.array) -> np.array:
     """
-    (Supplementary) - 
+    (Supplementary) - Returns the negative log likelihood for given arrays 
+    holding simulated data and a set of coefficients.
     """
     beta = beta.reshape((-1, 1)) # fixes tiny coef problem - need (4,1) not (4,)
     # P(error for each data point) calculated with p.m.f. for N(0, 1)
@@ -55,7 +56,9 @@ def negative_ll(beta: np.array, y_arr: np.array, x_arr: np.array) -> np.array:
 
 def estimate_mle(y: np.array, X: np.array) -> np.array:
     """
-    Exercise 2 - 
+    Exercise 2 - Estimates MLE coefficients for a given set of observations by 
+    finding the coefficients that minimize the negative log likelihood. Returns 
+    an array containing the coefficients.
     """
     betas_array = np.ones(4)
     X = np.c_[np.ones(X.shape[0]).reshape(-1, 1), X]
@@ -69,14 +72,17 @@ def estimate_mle(y: np.array, X: np.array) -> np.array:
 
 def sse(beta: np.array, y_arr: np.array, x_arr: np.array) -> np.array:
     """
-    (Supplementary) Returns the sum of squared error for given arrays of 
+    (Supplementary) Returns the sum of squared errors for given arrays holding
+    simulated data and a set of coefficients.
     """
     beta = beta.reshape((-1, 1)) # fixes tiny coef problem - need (4,1) not (4,)
     return np.sum((y_arr - (x_arr @ beta)) ** 2)
 
 def estimate_ols(y: np.array, X: np.array) -> np.array:
     """
-    Exercise 3 -
+    Exercise 3 - Estimates OLS coefficients for a given set of observations by 
+    finding the coefficients that minimize the sum of squared errors. Returns 
+    an array containing the coefficients.
     """
     betas_array = np.ones(4)
     X = np.c_[np.ones(X.shape[0]).reshape(-1, 1), X]
@@ -94,11 +100,11 @@ def main():
     problem set.
     """
     # Exercise 1
-    sim_data = simulate_data()
+    sim_data = simulate_data(323)
     print(f'Shape of simulated y array: {sim_data[0].shape}')
     print(f'Mean of simulate y array: {np.mean(sim_data[0])}')
     print(f'Shape of simulated X array: {sim_data[1].shape}')
-    print(f'Mean of simulate X array: {np.mean(sim_data[1])} \n')
+    print(f'Mean of simulate X array: {np.mean(sim_data[1])}')
 
     # Exercise 2
     ex_2_coef_array = estimate_mle(sim_data[0], sim_data[1]).x
