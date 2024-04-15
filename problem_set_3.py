d2f7721f2d2d2e0d049ea14ea3e13c4fa2e46f8f
zachkoverman <zachkoverman@gmail.com>
2024-04-14 17:27:00 -0700

    Setting up file

---
+++ problem_set_3.py
@@ -0,0 +1,31 @@
+"""
+Zach Koverman
+4/17/24
+ECON 481
+Problem Set 3
+
+This file contains functions written as exercises for Problem Set 3 in
+ECON 481 - Data Science Computing for Economics.
+"""
+
+import pandas as pd
+
+def github() -> str:
+    """
+    Exercise 0 - Returns the URL to this file in my GitHub repository for this
+    class.
+    """
+    return "https://github.com/zachkoverman/econ481/blob/main/problem_set_3.py"
+
+def import_yearly_data(years: list) -> pd.DataFrame:
+    """
+    Exercise 1 - 
+    """
+
+    return None
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
