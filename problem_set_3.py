ddb394fba565a5088b33e9c4d2131c617c8918cc
zachkoverman <zachkoverman@gmail.com>
2024-04-15 14:48:54 -0700

    Completed exercises 1 and 2, but still missing docstrings

--- problem_set_3.py
+++ problem_set_3.py
@@ -21,11 +21,53 @@ def import_yearly_data(years: list) -> pd.DataFrame:
     """
     Exercise 1 - 
     """
+    df_list = []
+    for target_year in years:
+        year_df = pd.read_excel("https://lukashager.netlify.app/econ-481/data" \
+                                f"/ghgp_data_{target_year}.xlsx",
+                                header=3)
+        year_df['year'] = target_year
+        df_list.append(year_df)
+    annual_emissions = pd.concat(df_list)
+
+    return annual_emissions
+
+def import_parent_companies(years: list) -> pd.DataFrame:
+    """
+    Exercise 2 - 
+    """
+    df_list = []
+    for target_year in years:
+        year_df = pd.read_excel("https://lukashager.netlify.app/econ-481/data" \
+                                "/ghgp_data_parent_company_09_2023.xlsb",
+                                sheet_name=str(target_year))
+        year_df.dropna(axis=0, how='all')
+        year_df['year'] = target_year
+        df_list.append(year_df)
+    company_emissions = pd.concat(df_list)
+
+    return company_emissions
+
+def n_null(df: pd.DataFrame, col: str) -> int:
+    """
+    Exercise 3 - 
+    """
+
 
     return None
 
 def main():
-    pass
+    """
+    Calls each function to demonstrate my solutions to the exercises in this 
+    problem set.
+    """
+    # Exercise 1
+    print(import_yearly_data([2021, 2022]).head(5))
+    print(import_yearly_data([2021, 2022]).describe())
+
+    # Exercise 2
+    print(import_parent_companies([2021, 2022]).head(5))
+    print(import_parent_companies([2021, 2022]).describe())
 
 if __name__ == "__main__":
     main()
