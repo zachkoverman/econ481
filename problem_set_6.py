c554229fbeb851a64d6335d936c7ccaa3717da34
zachkoverman <zachkoverman@gmail.com>
2024-05-15 09:34:50 -0700

    Setting up file to start work on problem set

---
+++ problem_set_6.py
@@ -0,0 +1,26 @@
+"""
+Zach Koverman
+5/15/24
+ECON 481
+Problem Set 6
+
+This file contains functions written as exercises for Problem Set 6 in
+ECON 481 - Data Science Computing for Economics
+"""
+
+def github() -> str:
+    """
+    Exercise 0 - Returns the URL to this file in my GitHub repository for this
+    class.
+    """
+    return "https://github.com/zachkoverman/econ481/blob/main/problem_set_6.py"
+
+def main():
+    """
+    Calls functions to demonstrate my solutions to the exercises in this 
+    problem set.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
